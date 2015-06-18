e6300ba4a42b6d8bb7e59810b67a8f16b5ea1a5e
nick <nicki@ca.ibm.com>
2015-06-18 10:25:41 -0400

    Added examples for Xalan and Saxon XSL transformers

--- qp.sh
+++ qp.sh
@@ -7,29 +7,32 @@
 ## Output   (optional) Path to the output file / directory.  
 ##          If not specified then a temporary directory is used.  Use '.' for the current directory.
 
-## Edit this to set path to your conversion program
+## NOTE: Edit the variable CONVPGM below to set path to your conversion program
 ## e.g. XSLT 1.0 compatible: /path/to/org.apache.xalan_2.7.1.v201005080400.jar  
 ## or XSLT 2.0 compatible: /another/path/to/saxon9he.jar
 # CONVPGM=xml2html.sh
 CONVPGM=
 
+## Example for using Xalan:
+# CONVPGM="java -cp /path/to/org.apache.xalan_2.7.1.jar:/path/to/org.apache.xerces_2.9.0.jar:/path/to/org.apache.xml.serializer_2.7.1.jar org.apache.xalan.xslt.Process"
+## Invocation in the function convert():
+# "${CONVPGM}" -IN "$1" -OUT "$3" -XSL "$2" -HTML
+
+## Example for using Saxon:
+# CONVPGM="java -cp /path/to/saxon9he.jar net.sf.saxon.Transform"
+## Invocation in the function convert():
+# "${CONVPGM}" -s:"$1" -xsl:"$2" -o:"$3"
+
 convert () {
 	# takes three arguments: XML file, XSL stylesheet, output file
 	if [[ -z "$CONVPGM" ]]; then
-		printf "\n\nPlease configure this script: set the variable CONVPGM to the path of your conversion program.\n"
-		printf "Hint: Apache Xalan and Saxonica Saxon JAR files can perform XSL transforms.\n\n"
+		printf "\n\nPlease configure this script by editing and setting the variable CONVPGM\nto the path of your conversion program.\n"
+		printf "Hint: Apache Xalan and Saxonica Saxon JAR files can perform XSL transforms.\nSee examples in this script's code.\n\n"
 		exit -1
 	fi
 
 	# this changes depending on the particular program too, as the way to pass arguments can be different
 	"${CONVPGM}" "$1" "$2" "$3"
-
-	# if using Xalan:
-	# "${CONVPGM}" -IN "$1" -OUT "$3" -XSL "$2" -HTML
-
-	# if using Saxon:
-	# "${CONVPGM}" -s:"$1" -xsl:"$2" -o:"$3"
-
 }
 
 ## TODO: Handle /h, /? etc...

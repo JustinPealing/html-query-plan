ac8850c989e19bbcd435c9ece286f33691f24dd2
Justin Pealing <jpealing@fiscaltec.com>
2016-09-20 10:37:27 +0100

    Add tests for estimated subtree cost.
    Sets up a basic test structure for finding nodes and asserting that their properties are set correctly.

--- test/qp_test.js
+++ test/qp_test.js
@@ -1,6 +1,27 @@
 import assert from 'assert';
 import QP from '../src/qp';
-var testPlan = require('raw!../test_plans/clustered index delete.sqlplan');
+var testPlan = require('raw!../test_plans/issue1.sqlplan');
+
+function findNodeById(container, nodeId) {
+    var nodes = container.querySelectorAll('.qp-node');
+    for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        if (getProperty(node, 'Node ID') == nodeId) {
+            return node;
+        }
+    }
+}
+
+function getProperty(node, key) {
+    var nodes = node.querySelectorAll('th');
+    for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        if (node.innerHTML === key) {
+            return node.parentNode.querySelector('td').innerHTML;
+        }
+    }
+    return null;
+}
 
 describe('qp.js', () => {
 
@@ -16,6 +37,16 @@ describe('qp.js', () => {
             assert.equal("0px", canvas.style.top);
             assert.equal("0px", canvas.style.left);
         });
+
+        it('Calculates estimated subtree cost correctly', () => {
+
+            var container = document.createElement("div");
+            QP.showPlan(container, testPlan);
+            
+            assert.equal("0.14839", getProperty(findNodeById(container, "1"), "Estimated Subtree Cost"));
+            assert.equal("0.0268975", getProperty(findNodeById(container, "18"), "Estimated Subtree Cost"));
+
+        })
 
     });
 

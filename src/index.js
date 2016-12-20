fde2432a446f877a53745d5848800714aa6c8a84
Justin Pealing <Justin Pealing>
2016-12-20 15:52:45 +0000

    Add jsTooltips option.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,32 @@ import { drawSvgLines } from './svgLines.js';
 import { initTooltip } from './tooltip.js';
 var qpXslt = require('raw!./qp.xslt');
 
-function showPlan(container, planXml) {
+function showPlan(container, planXml, options) {
+    var options = setDefaults(options, {
+        jsTooltips: true
+    });
+
     transform.setContentsUsingXslt(container, planXml, qpXslt);
     drawSvgLines(container);
-    initTooltip(container);
+
+    if (options.jsTooltips) {
+        initTooltip(container);
+    }
+}
+
+function setDefaults(options, defaults) {
+    var ret = {};
+    for (var attr in defaults) {
+        if (defaults.hasOwnProperty(attr)) {
+            ret[attr] = defaults[attr];
+        }
+    }
+    for (var attr in options) {
+        if (options.hasOwnProperty(attr)) {
+            ret[attr] = options[attr];
+        }
+    }
+    return ret;
 }
 
 module.exports.drawLines = drawSvgLines;

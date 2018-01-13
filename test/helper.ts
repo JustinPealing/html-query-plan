fdccb421532b662ce6271f6fe0144f8fa345ef4c
Justin Pealing <justin@pealing.me.uk>
2018-01-13 10:25:41 +0000

    Add type annotations to helper.ts

--- test/helper.ts
+++ test/helper.ts
@@ -1,6 +1,6 @@
 import * as QP from '../src/index';
 
-function findNodeById(container, nodeId, statementId?) {
+function findNodeById(container: Element, nodeId: string, statementId?: string) {
     let statmentElement = findStatmentElementById(container, statementId);
     let nodes = statmentElement.querySelectorAll('.qp-node');
     for (let i = 0; i < nodes.length; i++) {
@@ -11,58 +11,62 @@ function findNodeById(container, nodeId, statementId?) {
     }
 }
 
-function findStatmentElementById(container, statementId) {
+function findStatmentElementById(container: Element, statementId: string) {
     if (statementId) {
         return container.querySelector('div[data-statement-id="' + statementId + '"]');
     }
     return container.querySelector('.qp-tr');
 }
 
-function getProperty(node, key) {
+function getProperty(node: Element, key: string) {
     let nodes = node.querySelectorAll('th');
     for (let i = 0; i < nodes.length; i++) {
         let node = nodes[i];
         if (node.innerHTML === key) {
-            return node.parentNode.querySelector('td').innerHTML;
+            return node.parentElement.querySelector('td').innerHTML;
         }
     }
     return null;
 }
 
-function getToolTipSection(node, name) {
+function getToolTipSection(node: Element, name: string) {
     let titleNodes = node.querySelectorAll('.qp-bold');
     for (let i = 0; i < titleNodes.length; i++) {
         if (titleNodes[i].innerHTML == name) {
-            let next = titleNodes[i].nextSibling;
+            let next = <HTMLElement>titleNodes[i].nextElementSibling;
             return next.innerText || next.textContent;
         }
     }
     return null;
 }
 
-function getTooltipTitle(node) {
+function getTooltipTitle(node: Element) {
     let tt = node.querySelector('.qp-tt');
-    return tt.children[0].innerText;
+    return (<HTMLElement>tt.children[0]).innerText;
 }
 
-function getDescription(node) {
+function getDescription(node: Element) {
     let tt = node.querySelector('.qp-tt');
-    return tt.children[1].innerText;
+    return (<HTMLElement>tt.children[1]).innerText;
 }
 
-function getNodeLabel(node) {
-    return node.children[1].innerText;
+function getNodeLabel(node: Element) {
+    return getLabel(node, 1);
 }
 
-function getNodeLabel2(node) {
-    return node.children[2].innerText;
+function getNodeLabel2(node: Element) {
+    return getLabel(node, 2);
 }
 
-function getNodeLabel3(node) {
-    return node.children[3].innerText;
+function getNodeLabel3(node: Element) {
+    return getLabel(node, 3);
 }
 
-function showPlan(planXml) {
+function getLabel(node: Element, index: number) {
+    return (<HTMLElement>node.children[index]).innerText;
+}
+
+function showPlan(planXml: string) {
     let container = document.createElement("div");
     QP.showPlan(container, planXml);
     return container;

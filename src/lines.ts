fbf7e4ee6ca9ca880edc74152b0c2bf83a176d95
Justin Pealing <justin@pealing.me.uk>
2018-02-20 23:22:52 +0000

    Add unit test for path to draw arrow
    Also renamed some variables to try and clarify what everything is.

--- src/lines.ts
+++ src/lines.ts
@@ -18,11 +18,11 @@ function drawLines(container: Element) {
     }
 }
 
-/*
+/**
  * Enumerates all child nodes and draws line from those nodes to the given parent node.
- * @draw SVG drawing context to use.
- * @offset Bounding client rect of the root SVG context.
- * @parent Parent .qp-node element.
+ * @param draw SVG drawing context to use.
+ * @param offset Bounding client rect of the root SVG context.
+ * @param parent Parent .qp-node element.
  */
 function drawLinesForParent(draw: SVG.Doc, offset: ClientRect, parent: Element) {
     let children = findAncestor(parent, 'qp-tr').children[1].children;
@@ -34,64 +34,71 @@ function drawLinesForParent(draw: SVG.Doc, offset: ClientRect, parent: Element) 
 
 /**
  * Draws the arrow between two nodes.
- * @draw SVG drawing context to use.
- * @offset Bounding client rect of the root SVG context.
- * @fromElement Node element from which to draw the arrow (leftmost node).
- * @toElement Node element to which to draw the arrow (rightmost node).
+ * @param draw SVG drawing context to use.
+ * @param offset Bounding client rect of the root SVG context.
+ * @param parent Node element from which to draw the arrow (leftmost node).
+ * @param child Node element to which to draw the arrow (rightmost node).
  */
-function drawArrowBetweenNodes(draw: SVG.Doc, offset: ClientRect, fromElement: Element, toElement: Element) {
-    let fromOffset = fromElement.getBoundingClientRect();
-    let toOffset = toElement.getBoundingClientRect();
+function drawArrowBetweenNodes(draw: SVG.Doc, offset: ClientRect, parent: Element, child: Element) {
+    let parentOffset = parent.getBoundingClientRect();
+    let childOffset = child.getBoundingClientRect();
 
-    let fromX = fromOffset.right;
-    let fromY = (fromOffset.top + fromOffset.bottom) / 2;
+    let toX = parentOffset.right;
+    let toY = (parentOffset.top + parentOffset.bottom) / 2;
 
-    let toX = toOffset.left;
-    let toY = (toOffset.top + toOffset.bottom) / 2;
+    let fromX = childOffset.left;
+    let fromY = (childOffset.top + childOffset.bottom) / 2;
 
-    let midOffsetLeft = fromX / 2 + toX / 2;
+    let midOffsetLeft = toX / 2 + fromX / 2;
 
-    let fromPoint = {
-        x: fromX - offset.left + 1,
-        y: fromY - offset.top
-    };
     let toPoint = {
-        x: toOffset.left - offset.left - 1,
+        x: toX - offset.left + 1,
         y: toY - offset.top
     };
+    let fromPoint = {
+        x: childOffset.left - offset.left - 1,
+        y: fromY - offset.top
+    };
     let bendOffsetX = midOffsetLeft - offset.left;
-    drawArrow(draw, fromPoint, toPoint, bendOffsetX);
+    drawArrow(draw, toPoint, fromPoint, bendOffsetX);
 }
 
 /**
  * Draws an arrow between two points.
- * @draw SVG drawing context to use.
- * @from {x,y} coordinates of tail end.
- * @to {x,y} coordinates of the pointy end.
- * @bendX Offset from toPoint at which the "bend" should happen. (X axis) 
+ * @param draw SVG drawing context to use.
+ * @param from {x,y} coordinates of tail (flat) end.
+ * @param to {x,y} coordinates of the arrowhead (pointy) end.
+ * @param bendX Offset from toPoint at which the "bend" should happen. (X axis).
  */
-function drawArrow(draw: SVG.Doc, from: Point, to: Point, bendX: number) {
-
-    let points: any = [
-        [from.x, from.y],
-        [from.x + 3, from.y - 3],
-        [from.x + 3, from.y - 1],
-        [bendX + (from.y <= to.y ? 1 : -1), from.y - 1],
-        [bendX + (from.y <= to.y ? 1 : -1), to.y - 1],
-        [to.x, to.y - 1],
-        [to.x, to.y + 1],
-        [bendX + (from.y <= to.y ? -1 : 1), to.y + 1],
-        [bendX + (from.y <= to.y ? -1 : 1), from.y + 1],
-        [from.x + 3, from.y + 1],
-        [from.x + 3, from.y + 3],
-        [from.x, from.y]
-    ];
-    
+function drawArrow(draw: SVG.Doc, to: Point, from: Point, bendX: number) {
+    let points: any = arrowPath(to, from, bendX);
     draw.polyline(points).fill('#E3E3E3').stroke({
         color: '#505050',
         width: 0.5
     });
-
 }
 
-export { drawLines }
+/**
+ * Creates the path for an arrow between two points.
+ * @param to Coordinates of the the arrowhead (pointy) end.
+ * @param from mid-point of the tail (flat) end.
+ * @param bendX Offset from toPoint at which the "bend" should happen. (X axis).
+ */
+function arrowPath(to: Point, from: Point, bendX: number) {
+    return [
+        [to.x, to.y],
+        [to.x + 3, to.y - 3],
+        [to.x + 3, to.y - 1],
+        [bendX + (to.y <= from.y ? 1 : -1), to.y - 1],
+        [bendX + (to.y <= from.y ? 1 : -1), from.y - 1],
+        [from.x, from.y - 1],
+        [from.x, from.y + 1],
+        [bendX + (to.y <= from.y ? -1 : 1), from.y + 1],
+        [bendX + (to.y <= from.y ? -1 : 1), to.y + 1],
+        [to.x + 3, to.y + 1],
+        [to.x + 3, to.y + 3],
+        [to.x, to.y]
+    ];
+}
+
+export { drawLines, arrowPath }
---
+++ test/lines_test.ts
@@ -0,0 +1,49 @@
+import * as assert from 'assert';
+import { arrowPath } from '../src/lines';
+
+describe('lines.arrowPath', () => {
+
+    it('Returns an arrow path 12 segments long with a bend that starts and ends at `to`', () => {
+
+        let points:any = arrowPath({x:10,y:15}, {x:90,y:95}, 30);
+        assert.equal(12, points.length);
+
+        assert.equal(10, points[0][0]);
+        assert.equal(15, points[0][1]);
+        
+        assert.equal(13, points[1][0]);
+        assert.equal(12, points[1][1]);
+
+        assert.equal(13, points[2][0]);
+        assert.equal(14, points[2][1]);
+
+        assert.equal(31, points[3][0]);
+        assert.equal(14, points[3][1]);
+
+        assert.equal(31, points[4][0]);
+        assert.equal(94, points[4][1]);
+
+        assert.equal(90, points[5][0]);
+        assert.equal(94, points[5][1]);
+
+        assert.equal(90, points[6][0]);
+        assert.equal(96, points[6][1]);
+
+        assert.equal(29, points[7][0]);
+        assert.equal(96, points[7][1]);
+
+        assert.equal(29, points[7][0]);
+        assert.equal(96, points[7][1]);
+
+        assert.equal(13, points[9][0]);
+        assert.equal(16, points[9][1]);
+
+        assert.equal(13, points[10][0]);
+        assert.equal(18, points[10][1]);
+        
+        assert.equal(10, points[11][0]);
+        assert.equal(15, points[11][1]);
+
+    })
+
+})
